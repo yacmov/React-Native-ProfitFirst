9f7f1b4d1f26a5fd513f5be6bfc4bf102feeec22
Cedric Ko <yacmov@gmail.com>
2024-12-25 00:59:07 +0800

    feat: New account UI

--- app/(tabs)/accounts.tsx
+++ app/(tabs)/accounts.tsx
@@ -1,10 +1,17 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import { View, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AccountsList from "@/components/AccountsList";
 
 const AccountScreen = () => {
+  const [name, setName] = useState("");
+  const [cap, setCap] = useState("");
+  const [tap, setTap] = useState("");
+
+  const createAccount = () => {
+    console.warn("Create account: ", name, cap, tap);
+  };
+
   return (
     <SafeAreaView className="gap-2 px-4">
       <View className="flex-row justify-between px-4">
@@ -14,6 +21,12 @@ const AccountScreen = () => {
       </View>
 
       <AccountsList />
+      <View className="flex-row justify-between px-4">
+        <TextInput value={name} onChangeText={setName} placeholder="Name" />
+        <TextInput value={cap} onChangeText={setCap} placeholder="CAP %" />
+        <TextInput value={tap} onChangeText={setTap} placeholder="TAP %" />
+      </View>
+      <Button title="Add account" onPress={createAccount} />
     </SafeAreaView>
   );
 };
