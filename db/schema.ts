07e0a356a7f4f071638bed19a02df9957aeba9af
Cedric Ko <yacmov@gmail.com>
2025-01-02 00:05:03 +0800

    feat: Add timestamps to all models for data sync

--- db/schema.ts
+++ db/schema.ts
@@ -1,50 +1,37 @@
 import { appSchema, tableSchema } from "@nozbe/watermelondb";
 
 export default appSchema({
-  version: 4,
+  version: 5,
   tables: [
     tableSchema({
       name: "accounts",
       columns: [
+        { name: "created_at", type: "number" },
+        { name: "updated_at", type: "number" },
         { name: "name", type: "string" },
         { name: "cap", type: "number" },
         { name: "tap", type: "number" },
-        { name: "created_at", type: "number" },
-        { name: "updated_at", type: "number" },
         { name: "user_id", type: "string" },
       ],
     }),
     tableSchema({
       name: "allocations",
       columns: [
-        { name: "income", type: "number" },
         { name: "created_at", type: "number" },
+        { name: "updated_at", type: "number" },
+        { name: "income", type: "number" },
         { name: "user_id", type: "string" },
       ],
     }),
     tableSchema({
       name: "account_allocations",
       columns: [
-        {
-          name: "created_at",
-          type: "number",
-        },
-        {
-          name: "account_id",
-          type: "string",
-        },
-        {
-          name: "allocation_id",
-          type: "string",
-        },
-        {
-          name: "amount",
-          type: "number",
-        },
-        {
-          name: "cap",
-          type: "number",
-        },
+        { name: "created_at", type: "number" },
+        { name: "updated_at", type: "number" },
+        { name: "account_id", type: "string" },
+        { name: "allocation_id", type: "string" },
+        { name: "amount", type: "number" },
+        { name: "cap", type: "number" },
         { name: "user_id", type: "string" },
       ],
     }),
--- model/Account.ts
+++ model/Account.ts
@@ -1,8 +1,17 @@
 import { Model } from "@nozbe/watermelondb";
-import { field, nochange, text } from "@nozbe/watermelondb/decorators";
+import {
+  date,
+  field,
+  nochange,
+  readonly,
+  text,
+} from "@nozbe/watermelondb/decorators";
 
 export default class Account extends Model {
   static table = "accounts";
+
+  @readonly @date("created_at") createdAt!: Date;
+  @readonly @date("updated_at") updatedAt!: Date;
 
   @text("name") name!: string;
   @field("cap") cap!: number;
